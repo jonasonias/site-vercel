dc7de09afdab98a2b268ceb356795fd7cc292930
Jonas <jonasonias@gmail.com>
2023-06-12 19:02:42 -0300

    link

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import Link from "next/link"
+
+import Head from "next/head"
+import Image from "next/image"
+
 export default function Home(){
-    return <div>Página Home</div>
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link href="/products">
+                        Produtos
+                    </Link>
+                </li>
+            </ul>
+            <h1>Home</h1>
+        </div>
+    )
 }
