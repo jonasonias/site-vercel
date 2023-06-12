e3d2cc50d0f2f3e820fea976e1807580388de942
Jonas <jonasonias@gmail.com>
2023-06-12 19:36:53 -0300

    navbar full

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
+import Navbar from "../components/Navbar"
 import Link from "next/link"
 
 export default function About(){
     return (
         <>
+        <Navbar />
         <h1>Sobre</h1>
         <Link href="/">
             Home
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,8 +1,10 @@
+import Navbar from "../../components/Navbar"
 import Link from "next/link"
 
 export default function Products(){
     return (
         <>
+        <Navbar />
         <h1>Página Produtos</h1>
         <Link href="/">
             Home
