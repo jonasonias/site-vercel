c9da99174be6a9b577d3fa4aec32fc5b107347c6
Jonas <jonasonias@gmail.com>
2023-06-13 01:27:59 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 
+import { Analytics } from '@vercel/analytics/react';
 import MainContainer from '../components/MainContainer'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MainContainer>
-      <Component {...pageProps} />
-    </MainContainer>
+    <>
+      <MainContainer>
+        <Component {...pageProps} />
+      </MainContainer>
+      <Analytics />
+    </>
   )
 }
 
