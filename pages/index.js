53872a4d61197db0fe362a90e968c5cee76e6ae3
Jonas <jonasonias@gmail.com>
2023-06-21 00:55:21 -0300

    img

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
-
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -14,6 +14,12 @@ export default function Home() {
         ></meta>
       </Head>
       <h1 className={styles.title}>Página principal</h1>
+      <Image
+        src="/images/img1.jpg" 
+        width={250}
+        height={200}
+        alt='Imagem Teste'
+      />
     </>
   )
 }
