c2fd9e536a9fb69be99fb4e831349f0ab721b689
Jonas <jonasonias@gmail.com>
2023-06-27 00:14:56 -0300

    404

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound(){
+
+    return(
+        <>
+            <h1>404 Page Not Found</h1>
+            <Link href="/">
+                Ir para página Principal
+            </Link>
+        </>
+    )
+}
